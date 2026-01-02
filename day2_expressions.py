534320fad35d41751f9c2596816e977b66ddfd36
Harshvardhan Kapoor <harshvardhankapoor13@gmail.com>
2026-01-02 16:26:19 +0530

    Day 2: expressions and calculations

---
+++ day2_expressions.py
@@ -0,0 +1,20 @@
+# Program 1: Add two numbers
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+print("Sum:", a + b)
+
+print("--------------")
+
+# Program 2: Celsius to Fahrenheit
+c = float(input("Enter temperature in Celsius: "))
+f = (c * 9/5) + 32
+print("Fahrenheit:", f)
+
+print("--------------")
+
+# Program 3: Simple Interest
+p = float(input("Enter principal: "))
+r = float(input("Enter rate: "))
+t = float(input("Enter time: "))
+si = (p * r * t) / 100
+print("Simple Interest:", si)
